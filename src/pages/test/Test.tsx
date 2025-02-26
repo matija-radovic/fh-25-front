fdf6b9377c56552b954d90a5746e684f4bd9e2e3
Tamara Cvejic <tamaracvejic45@gmail.com>
2025-02-26 12:43:46 +0100

    Agenda commit

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,3 +1,4 @@
+import Agenda from "../../components/Agenda/Agenda";
 import './Test.scss'
 
 /**
@@ -7,7 +8,7 @@ import './Test.scss'
 const Test = () => {
   return (
     <>
-      {/* Put your component here */}
+      <Agenda/>
     </>
   )
 }
