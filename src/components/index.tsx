aa4a4882b40650661a2b1b80c9d1499d62a243ad
matija-radovic <mr20200227@student.fon.bg.ac.rs>
2025-04-24 04:42:24 +0200

    Rewrite

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,8 @@ import Layout from "./-layout/Layout";
 export { Layout }
 
 import Section from "./-shared/Section/Section";
-export { Section }
+import Slider from "./-shared/Slider/Slider";
+export { Section, Slider }
 
 import ExampleComponent from "./Example/Example";
 export { ExampleComponent }
--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,5 +1,4 @@
-import Slider from "../../components/-shared/Section/slider/Slider";
-import "./Test.scss";
+import './Test.scss'
 
 /**
  * This component should be used to test your components, shared components or pages.
@@ -8,18 +7,9 @@ import "./Test.scss";
 const Test = () => {
   return (
     <>
-      <Slider
-        values={[
-          "https://picsum.photos/id/10/367/367",
-          "https://picsum.photos/id/11/367/367",
-          "https://picsum.photos/id/12/367/367",
-          "https://picsum.photos/id/13/367/367",
-          "https://picsum.photos/id/14/367/367",
-          "https://picsum.photos/id/15/367/367"
-        ]}
-      ></Slider>
+      {/* Put your component here */}
     </>
-  );
-};
+  )
+}
 
-export default Test;
+export default Test
