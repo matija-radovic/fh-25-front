dbe6037422efcdcb6002e694d39c3a52fd95f6e0
matija-radovic <mr20200227@student.fon.bg.ac.rs>
2025-06-06 21:30:13 +0200

    reverted Test.tsx

--- src/pages/test/Test.tsx
+++ src/pages/test/Test.tsx
@@ -1,20 +1,15 @@
-
-import OrgTeam2 from "../../components/OrgTeam/OrgTeam";
-import { orgTeams } from "../../utils/constants/orgteam/teams";
-import "./Test.scss";
+import './Test.scss'
 
 /**
  * This component should be used to test your components, shared components or pages.
  * Inside of the `Test.scss` are predefined styles just delete the `/*` to apply them
  */
-
 const Test = () => {
   return (
     <>
-      <OrgTeam2 teams={orgTeams} />
-      
+      {/* Put your component here */}
     </>
-  );
-};
+  )
+}
 
-export default Test;
+export default Test
