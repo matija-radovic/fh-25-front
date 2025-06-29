d91fab2a5e7f57245ee8f02e78a29bbc41f3a020
matija-radovic <mr20200227@student.fon.bg.ac.rs>
2025-06-29 16:23:57 +0200

    added correcthexagons

--- src/components/Signup/SignupSection.tsx
+++ src/components/Signup/SignupSection.tsx
@@ -1,26 +1,28 @@
 import { Link } from "react-router-dom";
 import TextBox from "../-shared/TextBox/TextBox";
 import "./SignupSection.scss";
-import hexagon from "../../assets/hexagon.svg";
 import { Section } from "../"
-
-
+import Hexagon from "../-shared/Hexagon/Hexagon";
 
 const SignupSection = () => {
   return (
     <Section className="signup-section">
+      <div className="hexagon-group one">
+        <Hexagon className="medium" />
+        <Hexagon className="small top-right" />
+      </div>
+      <div className="hexagon-group two">
+        <Hexagon className="medium" />
+        <Hexagon className="small bottom-left" />
+      </div>
       <TextBox className="text-box-w-650 signup-text-box" variant="white">
-        <img className="hexagon hex1" src={hexagon} alt="" />
-        <img className="hexagon hex2" src={hexagon} alt="" />
-        <img className="hexagon hex3" src={hexagon} alt="" />
-        <img className="hexagon hex4" src={hexagon} alt="" />
         <h3>ČEKAMO TVOJU PRIJAVU!</h3>
         <p>
           Prijavi se i budi deo... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla egestas interdum nisi, mollis varius nisi accumsan vitae. Aliquam at velit vel nunc ullamcorper dictum.
         </p>
         <Link to="/prijava" className="signup-button">
           PRIJAVI SE
-          </Link>
+        </Link>
         {/*<a href="#" className="signup-button">PRIJAVI SE</a>*/}
       </TextBox>
     </Section>
